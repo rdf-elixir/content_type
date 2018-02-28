e8e43b941700c2afda178183594d6231159accbc
Marcel Otto <marcelotto@gmx.de>
2018-02-28 23:16:39 +0100

    Initial commit
    	$ mix new content_type --module ContentType

---
+++ lib/content_type.ex
@@ -0,0 +1,18 @@
+defmodule ContentType do
+  @moduledoc """
+  Documentation for ContentType.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ContentType.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/content_type_test.exs
@@ -0,0 +1,8 @@
+defmodule ContentTypeTest do
+  use ExUnit.Case
+  doctest ContentType
+
+  test "greets the world" do
+    assert ContentType.hello() == :world
+  end
+end
