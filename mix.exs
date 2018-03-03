2b75e8bd8e527275004aaa27f5e04442b249c9cc
Marcel Otto <marcelotto@gmx.de>
2018-03-03 22:46:20 +0100

    Update Mixfile

--- mix.exs
+++ mix.exs
@@ -1,28 +1,56 @@
 defmodule ContentType.MixProject do
   use Mix.Project
 
+  @repo_url "https://github.com/marcelotto/content_type"
+
+  @version "0.1.0"
+
   def project do
     [
       app: :content_type,
-      version: "0.1.0",
-      elixir: "~> 1.6",
+      version: @version,
+      elixir: "~> 1.3",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex
+      package: package(),
+      description: description(),
+
+      # Docs
+      name: "ContentType",
+      docs: [
+        main: "ContentType",
+        source_url: @repo_url,
+        source_ref: "v#{@version}",
+        extras: ["README.md"],
+      ],
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  defp description do
+    """
+    A parser for HTTP Content-Type headers.
+    """
+  end
+
+  defp package do
     [
-      extra_applications: [:logger]
+      licenses: ["Apache 2"],
+      maintainers: ["Marcel Otto"],
+      links: %{"GitHub" => @repo_url},
+      files: ~w[lib src mix.exs README.md CHANGELOG.md LICENSE .formatter.exs]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  def application do
+    [extra_applications: [:logger]]
+  end
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, "~> 0.17.1", only: :dev, runtime: false},
+      {:inch_ex, ">= 0.0.0", only: [:dev, :test]},
     ]
   end
 end
